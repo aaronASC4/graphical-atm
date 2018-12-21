c40d6cb556b43c2636da2295995e21df046617d5
aaronASC4 <aarongraves07@gmail.com>
2018-12-21 18:16:49 -0500

    Update HomeView.java

--- src/view/HomeView.java
+++ src/view/HomeView.java
@@ -1,84 +1,88 @@
 package view;
-
+ 
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-
+ 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+ 
 import controller.ViewManager;
-
+ 
 @SuppressWarnings("serial")
 public class HomeView extends JPanel implements ActionListener {
-	
-	private ViewManager manager;		// manages interactions between the views, model, and database
-	
-	/**
-	 * Constructs an instance (or objects) of the HomeView class.
-	 * 
-	 * @param manager
-	 */
-	
-	public HomeView(ViewManager manager) {
-		super();
-		
-		this.manager = manager;
-		initialize();
-	}
-	
-	///////////////////// PRIVATE METHODS /////////////////////////////////////////////
-	
-	/*
-	 * Initializes the HomeView components.
-	 */
-	
-	private void initialize() {
-		
-		// TODO
-		//
-		// this is a placeholder for this view and should be removed once you start
-		// building the HomeView.
-		
-		this.add(new javax.swing.JLabel("HomeView", javax.swing.SwingConstants.CENTER));
-		
-		// TODO
-		//
-		// this is where you should build the HomeView (i.e., all the components that
-		// allow the user to interact with the ATM - deposit, withdraw, transfer, etc.).
-		//
-		// feel free to use my layout in LoginView as an example for laying out and
-		// positioning your components.
-	}
-	
-	/*
-	 * HomeView is not designed to be serialized, and attempts to serialize will throw an IOException.
-	 * 
-	 * @param oos
-	 * @throws IOException
-	 */
-	
-	private void writeObject(ObjectOutputStream oos) throws IOException {
-		throw new IOException("ERROR: The HomeView class is not serializable.");
-	}
-	
-	///////////////////// OVERRIDDEN METHODS //////////////////////////////////////////
-	
-	/*
-	 * Responds to button clicks and other actions performed in the HomeView.
-	 * 
-	 * @param e
-	 */
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		// TODO
-		//
-		// this is where you'll setup your action listener, which is responsible for
-		// responding to actions the user might take in this view (an action can be a
-		// user clicking a button, typing in a textfield, etc.).
-		//
-		// feel free to use my action listener in LoginView.java as an example.
-	}
+   
+    private ViewManager manager;        // manages interactions between the views, model, and database
+    private JLabel errorMessageLabel;       // label for potential error messages
+    private JButton logoutButton;
+   
+    public HomeView(ViewManager manager) {
+        super();
+       
+        this.manager = manager;
+        this.errorMessageLabel = new JLabel("", SwingConstants.CENTER);
+        initialize();
+    }
+   
+    ///////////////////// PRIVATE METHODS /////////////////////////////////////////////
+   
+    /*
+     * Initializes the HomeView components.
+     */
+   
+    private void initialize() {
+       
+        this.setLayout(null);
+       
+        initLogoutButton();
+       
+        this.add(new javax.swing.JLabel("HomeView", javax.swing.SwingConstants.CENTER));
+       
+    }
+ 
+    private void initLogoutButton() {
+        logoutButton = new JButton();
+        logoutButton.setBounds(5, 5, 50, 50);
+        logoutButton.addActionListener(this);
+       
+        try {
+            Image image = ImageIO.read(new File("images/logout.ico"));
+            logoutButton.setIcon(new ImageIcon(image));
+        } catch (Exception e) {
+            logoutButton.setText("OFF");
+        }
+       
+        this.add(logoutButton);
+    }
+   
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        throw new IOException("ERROR: The HomeView class is not serializable.");
+    }
+   
+    ///////////////////// OVERRIDDEN METHODS //////////////////////////////////////////
+   
+    /*
+     * Responds to button clicks and other actions performed in the HomeView.
+     *
+     * @param e
+     */
+   
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+       
+        if (source.equals(logoutButton)) {
+            manager.logout();
+        }
+    }
 }
